01375124a360bd488e6d5d21a51dd68081359e06
rahuldev7583 <rahuldev7583@gmail.com>
2026-02-05 09:17:42 +0530

    initial producer consumer added

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -1,3 +1,22 @@
+use shaq::Consumer;
+use std::fs::File;
+
+#[derive(Debug, Copy, Clone)]
+struct ShareState {
+    data: [u8; 512],
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Consumer");
+
+    let file = File::options()
+        .read(true)
+        .write(true)
+        .open("./../test")
+        .unwrap();
+
+    let mut consumer: Consumer<ShareState> = unsafe { Consumer::join(&file).unwrap() };
+    let value = consumer.try_read().unwrap();
+
+    println!("the value is : {:?}", value);
 }
--- producer/src/main.rs
+++ producer/src/main.rs
@@ -1,3 +1,35 @@
+use shaq::Producer;
+use std::fs::File;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+struct ShareState {
+    data: [u8; 512],
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Producer");
+
+    let file = File::options()
+        .create(true)
+        .read(true)
+        .write(true)
+        .truncate(true)
+        .open("./../test")
+        .unwrap();
+
+    println!("file created");
+
+    let mut producer: Producer<ShareState> =
+        unsafe { Producer::create(&file, 1024 * 1024) }.unwrap();
+
+    producer.sync();
+    unsafe {
+        let mut slot = producer.reserve().unwrap();
+
+        slot.as_mut().data.fill(24);
+    }
+
+    producer.commit();
+    println!("producer committed");
 }
